402a189be6a3484f9a13065953888ac41a11c0af
Karim Slimani <karim@playmedia.fr>
2017-09-01 13:02:04 +0200

    feat: use Spatie permission middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,6 +52,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'permission' => \App\Http\Middleware\CheckPermissionsMiddleware::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
     ];
 }
