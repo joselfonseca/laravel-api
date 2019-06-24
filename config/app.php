adf2fcc1d32dc86d354dfe56d64b9a50a0e3026d
Jose Luis Fonseca <jose@ditecnologia.com>
2019-06-24 00:49:28 +0000

    Apply fixes from StyleCI

--- config/app.php
+++ config/app.php
@@ -149,7 +149,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\ErrorHandlerServiceProvider::class
+        App\Providers\ErrorHandlerServiceProvider::class,
     ],
     /*
     |--------------------------------------------------------------------------
