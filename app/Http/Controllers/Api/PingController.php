ae5c465ae4537cd8f7bf442ee6ad29d8bf3c66e4
Jose Luis Fonseca <jose@ditecnologia.com>
2019-12-05 12:13:16 -0500

    add ip to ping endpoint for testing

--- app/Http/Controllers/Api/PingController.php
+++ app/Http/Controllers/Api/PingController.php
@@ -24,6 +24,7 @@ class PingController extends Controller
         return $this->response->array([
             'status' => 'ok',
             'timestamp' => \Carbon\Carbon::now(),
+            'host' => request()->ip()
         ]);
     }
 }
