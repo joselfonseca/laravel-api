28fbaa153ca8ad3ebac2e5628ebe652976a568fa
joselfonseca <jose.fonseca@domandtom.com>
2021-01-01 21:42:13 -0500

    clean up and set the default guard to API

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -36,13 +36,13 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
+        'api' => [
+            'driver' => 'passport',
             'provider' => 'users',
         ],
 
-        'api' => [
-            'driver' => 'passport',
+        'web' => [
+            'driver' => 'session',
             'provider' => 'users',
         ],
     ],
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\ApiDocsController::class, 'index']);
+Route::view('/', 'apidocs');
